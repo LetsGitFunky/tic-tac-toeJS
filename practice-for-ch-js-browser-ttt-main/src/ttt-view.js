c4f277f69c01eb8fc295d5bc0c494c3552b2bb34
LetsGitFunky <NickHeinBass@gmail.com>
2023-05-23 16:02:50 -0700

    handling clicks

--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,37 +1,34 @@
 class View {
   constructor(game, el) {
     this.game = game;
-    this.el = el; 
+    this.el = el;
     this.setupBoard();
+    this.handleClick(el);
   }
-  
+
   setupBoard() {
     let box = document.createElement("ul");
-    // console.log(box);
-  
-    // let item = document.createElement("li");
-
     for(let i = 0; i < 3; i ++) {
       for (let j = 0; j < 3; j ++) {
-        // box.push(document.createElement("li"));
-        // box.createElement("li");
         let square = document.createElement("li");
+        let pos = [i, j];
+        square.classList.add(pos);
         box.append(square);
-
       }
     }
-
     this.el.append(box);
   }
 
 
-  
+
   handleClick(e) {
+    // EventTarget.addEventListener("click", ()=> alert("hello"))
+    e.addEventListener("click", ()=> alert("click!"))
   }
 
   makeMove(square) {
   }
-  
+
   handleGameOver() {
   }
 }
