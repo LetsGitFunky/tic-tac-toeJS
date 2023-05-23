76daefd3e878a3f51daf61e1b3adf9783700840c
LetsGitFunky <NickHeinBass@gmail.com>
2023-05-23 12:21:19 -0700

    lunch break

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,7 @@
-Add your import statements for View and Game here
+// Add your import statements for View and Game here
+import View from "./ttt-view";
+import Game from "../ttt_node/game";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const game = new Game();
 });
