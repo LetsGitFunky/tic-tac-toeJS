1649fe11ba3f1dd24093c24152e931665838c113
Ahmed <ahmed96razi39@gmail.com>
2023-05-23 17:07:02 -0700

    last push

--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,9 +1,14 @@
+import Game from "../ttt_node/game";
+
 class View {
   constructor(game, el) {
     this.game = game;
     this.el = el;
     this.setupBoard();
-    this.handleClick(el);
+    // this.handleClick(el);
+    this.el.addEventListener("click", this.handleClick.bind(this)); 
+      // let move = this.game.playMove(sq);
+    
   }
 
   setupBoard() {
@@ -12,18 +17,29 @@ class View {
       for (let j = 0; j < 3; j ++) {
         let square = document.createElement("li");
         let pos = [i, j];
-        square.classList.add(pos);
+        // square.pos = [pos];
+        // square.classList.add(pos);
+        square.dataset.pos = [i, j];
         box.append(square);
       }
     }
+    // box or square square.pos = box.pos = [i, jj]
     this.el.append(box);
   }
 
 
-
   handleClick(e) {
     // EventTarget.addEventListener("click", ()=> alert("hello"))
-    e.addEventListener("click", ()=> alert("click!"))
+    // let el = getElementsByClassName('li.class');
+    // el.addEventListener("click", ()=> {
+    //   let move = this.game.playMove(sq);
+
+    // });
+    if (e.target.nodeName === "LI") {
+      // console.log(e.target.dataset.pos);
+      this.game.playMove(e.target.dataset.pos.split(','));
+      console.log("X");
+    }
   }
 
   makeMove(square) {
